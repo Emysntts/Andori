768302e9f58da473be3c966d76a06ac058c7ab46
Maria Clara Dantas <claradantas@Marias-MacBook-Air.local>
2025-11-09 04:51:35 -0300

    rota de feedback

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -5,6 +5,7 @@ from .routes.turmas import router as turmas_router
 from .routes.aulas import router as aulas_router
 from .routes.description import router as description_router
 from .routes.familydata import router as familydata_router
+from .routes.feedback import router as feedback_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(material_router)
@@ -13,5 +14,6 @@ api_router.include_router(turmas_router)
 api_router.include_router(aulas_router)
 api_router.include_router(description_router)
 api_router.include_router(familydata_router)
+api_router.include_router(feedback_router)
 
 
